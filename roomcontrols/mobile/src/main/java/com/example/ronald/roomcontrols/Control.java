280a234466237f1912e5241e0e89812a5c5c7b93
RonZenUbu <ronladred@gmail.com>
2015-09-15 20:15:06 -0400

    Android add was added with the structure of the layouts and classes made

---
+++ roomcontrols/mobile/src/main/java/com/example/ronald/roomcontrols/Control.java
@@ -0,0 +1,15 @@
+package com.example.ronald.roomcontrols;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ronald on 9/15/15.
+ */
+public class Control extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controls);
+    }
+}
---
+++ roomcontrols/mobile/src/main/java/com/example/ronald/roomcontrols/SettingsMenu.java
@@ -0,0 +1,35 @@
+package com.example.ronald.roomcontrols;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.Set;
+
+/**
+ * Created by ronald on 9/15/15.
+ */
+public class SettingsMenu extends AppCompatActivity{
+
+    private BluetoothAdapter myBluetooth = null;
+    private Set pairedDevices;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if (myBluetooth == null){
+            Toast.makeText(getApplicationContext(), "Bluetooth Devide not available", Toast.LENGTH_LONG);
+            finish();
+        }
+        else {
+            if(myBluetooth.isEnabled()){}
+            else{
+                Intent turnToothOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnToothOn, 1);
+            }
+        }
+    }
+}
