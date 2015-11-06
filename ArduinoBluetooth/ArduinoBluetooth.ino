fe0fff1526570a564a43ce99ba2cb079f5c79010
RonZenUbu <ronladred@gmail.com>
2015-11-06 12:28:42 -0500

    finshed void loop and flashOP

--- ArduinoBluetooth/ArduinoBluetooth.ino
+++ ArduinoBluetooth/ArduinoBluetooth.ino
@@ -10,9 +10,9 @@
 #define AL 13 // Turning alarm on/off
 
 #define cmdL 'L'      //UART-command for the lights
-#define cmdA 'A'      //UART-command for the air conditioner
-#define cmdH 'H'      //UART-command for the time of hours
-#define cmdM 'M'      //UART-command for the time of minutes 
+#define cmdA 'A'      //UART-command for the air conditioner or heater
+#define cmdH 'H'      //UART-command for the time of hours for the alarm
+#define cmdM 'M'      //UART-command for the time of minutes for the alarm
 #define cmdF 'F'      // UART-command for EEPROM operation
 #define cmdr 'r'      // UART-command for EEPROM operation (read)
 #define cmdw 'w'      // UART-command for EEPROM operation (write)
@@ -34,7 +34,7 @@ char command;      // command
 unsigned long currentTime, lastTimeCommand, autoOFF; //timekeeping variable 
 // TODO: change last time command to daily time reseter
 
-void(setup(){
+void setup(){
   Serial.begin(115200);   //Initialization of UART, the number represents the baud number that the bluetooth device runs at 
   // initializing of all output 
   pinMode(L1, OUTPUT);
@@ -44,6 +44,116 @@ void(setup(){
   pinMode(AC, OUTPUT);
   pinMode(AL, OUTPUT); // TODO change AL to be the whether the alarm can get set off, it is currently set to turn the alarm off/on
   timer_init();        // Initialise timer
+  
 }
+void timer_init(){
+ uint8_t sw_autoOFF = EEPROM.read(0); // read EEPROM whether the device is connected or not.
+ if( sw_autoOFF == '1'){
+   char var_Data[3];
+   var_Data[0] = EEPROM.read(1);
+   var_Data[1] = EEPROM.read(2);
+   var_Data[2] = EEPROM.read(3);
+   autoOFF = atoi(var_Data)*100; // variable autoOFF ms 
+ }
+ else if (sw_autoOFF == '0'){
+  autoOFF = 999999; 
+ }
+ else if (sw_autoOFF == 255){
+  autoOFF = 2500; //  
+ }
+  currentTime = millis(); // reads eclapsed time 
+}
+
+void loop(){
+  if(Serial.available() > 0){   //if data is being transmitted 
+   incomingByte = Serial.read(); // then read the data
+   if(incomingByte == cmdL){ // read the data if it was for the light
+     command = cmdL;
+     memset(L_Data,0,sizeof(L_Data));
+     L_index = 0;
+   
+   }
+   else if (incomingByte == cmdA){ // read air conditioner or heater data
+     command = cmdA;
+     memset(A_Data,0,sizeof(A_Data));
+     A_index = 0;
+   
+   }
+   else if( incomingByte == cmdH){  // read hours data
+    command = cmdH;
+    memset(H_Data,0,sizeof(H_Data));
+    H_index = 0; 
+   }
+   else if( incomingByte == cmdM){ // read minutes data
+    command = cmdM; 
+    memset(F_Data,0,sizeof(F_Data));
+    F_index = 0;
+   }
+   else if(incomingByte == cmdF){
+    command = cmdF;
+    memset(F_Data,0,sizeof(F_Data));
+    F_index = 0; 
+   }
+   else if(incomingByte  == '\r')command = 'e'; // end of line
+   else if(incomingByte == '\t') command = 't'; // end of line for EEPROM 
+   
+   if( command ==cmdL && incomingByte != cmdL){
+    L_Data[L_index] = incomingByte;
+    L_index++; 
+   }
+   else if( command = cmdA && incomingByte != cmdA){
+     A_Data[A_index] = incomingByte;   
+     A_index++;  
+   }
+   else if( command = cmdH && incomingByte != cmdH){
+     H_Data[H_index] = incomingByte;
+     H_index++;
+   }
+   else if ( command = cmdM && incomingByte != cmdM){
+     M_Data[M_index] = incomingByte;
+     M_index++;
+   }
+   else if ( command = cmdF && incomingByte != cmdF){
+     F_Data[F_index] = incomingByte;
+     F_index++;
+   }
+   else if ( command == 'e'){ 
+     //TODO:finish command of the finished and make the hardware update method
+ 
+   }
+   else if( command == 't'){
+     Flash_Op(F_Data[0],F_Data[1],F_Data[2],F_Data[3],F_Data[4]);
+
+     
+   }   
+   
+   lastTimeCommand = millis();
+  }
+  if(millis() >= lastTimeCommand + autoOFF){
+    //TODO create hardware command
+  }
+  
+}
+
+void Flash_Op(char FCMD, uint8_t z1, uint8_t z2, uint8_t z3, uint8_t z4){
+
+  if(FCMD == cmdr){           // if EEPROM data read command
+    Serial.print("FData:");       // send EEPROM data
+    Serial.write(EEPROM.read(0));     // read value from the memory with 0 address and print it to UART
+    Serial.write(EEPROM.read(1));
+    Serial.write(EEPROM.read(2));
+    Serial.write(EEPROM.read(3));
+    Serial.print("\r\n");         // mark the end of the transmission of data EEPROM
+  }
+  else if(FCMD == cmdw){          // if EEPROM data write command
+    EEPROM.write(0,z1);               // z1 record to a memory with 0 address
+    EEPROM.write(1,z2);
+    EEPROM.write(2,z3);
+    EEPROM.write(3,z4);
+    timer_init();             // reinitialize the timer
+    Serial.print("FWOK\r\n");         // send a message that the data is successfully written to EEPROM
+  }
+}
+
 
 
